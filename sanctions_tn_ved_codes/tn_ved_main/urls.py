4df496a170bd51e85b7b2af1b4d5a63d5e0fb0c4
Ivan <ivanrakach@gmail.com>
2022-03-28 18:03:17 +0300

    - search function have been done

--- sanctions_tn_ved_codes/tn_ved_main/urls.py
+++ sanctions_tn_ved_codes/tn_ved_main/urls.py
@@ -4,4 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('', views.index, name="home"),
+    path('search_results/', SearchResultsView.as_view(), name='search_results'),
+    # path('search/', views.show_search_results, name='search_results'),
 ]
--- sanctions_tn_ved_codes/tn_ved_main/views.py
+++ sanctions_tn_ved_codes/tn_ved_main/views.py
@@ -1,6 +1,9 @@
+from django.views.generic import CreateView, ListView, FormView
+from django.db.models import Q
 from django.shortcuts import render
 
 # Create your views here.
+from .models import *
 
 
 def index(request):
@@ -9,3 +12,22 @@ def index(request):
         'title': 'Проверка кодов ТН ВЭД'
     }
     return render(request, 'tn_ved_main/index.html', context=param_for_render)
+
+# def show_search_results(request):
+#
+#     param_for_render = {
+#
+#         'title': 'Результаты поиска'
+#     }
+#     return render(request, 'tn_ved_main/search_results.html', context=param_for_render)
+
+
+class SearchResultsView(ListView):
+    model = ActualSanctionsTnvedCodes
+    template_name = 'search_results.html'
+
+    def get_queryset(self):  # новый
+        query = self.request.GET.get('q')
+        object_list = ActualSanctionsTnvedCodes.objects.filter(sanctions_tn_ved_code=query)
+        return object_list
+
