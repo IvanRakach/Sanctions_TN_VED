176e6de5f6c84cda2bfb3218bc19ec5b7f961503
Ivan <ivanrakach@gmail.com>
2022-03-28 13:49:23 +0300

    - models have been made - migrations have been made - models have been registered in admin panel

--- sanctions_tn_ved_codes/tn_ved_main/admin.py
+++ sanctions_tn_ved_codes/tn_ved_main/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+# from .forms import *
+
+
+class ActualTnvedDirectoryAdmin(admin.ModelAdmin):
+    # список полей, которые мы хотим видеть в нашей админ панели
+    list_display = ('id', 'tn_ved_code', 'tn_ved_code_description', )
+    # те поля, на которые мы можем кликнуть и перейти
+    list_display_links = ('id', 'tn_ved_code')
+    # те поля, по которым мы можем производить поиск информации
+    search_fields = ('tn_ved_code', 'tn_ved_code_description', )
+
+
+admin.site.register(ActualTnvedDirectory, ActualTnvedDirectoryAdmin)
+
+
+class ActualSanctionsTnvedCodesAdmin(admin.ModelAdmin):
+    # список полей, которые мы хотим видеть в нашей админ панели
+    list_display = ('id', 'sanctions_tn_ved_code', 'sanctions_tn_ved_code_description', 'country_code_2_dig', )
+    # те поля, на которые мы можем кликнуть и перейти
+    list_display_links = ('id', 'sanctions_tn_ved_code')
+    # те поля, по которым мы можем производить поиск информации
+    search_fields = ('sanctions_tn_ved_code', 'sanctions_tn_ved_code_description', )
+
+
+admin.site.register(ActualSanctionsTnvedCodes, ActualSanctionsTnvedCodesAdmin)
+
+
+class CountriesAgainstRFRBAdmin(admin.ModelAdmin):
+    # список полей, которые мы хотим видеть в нашей админ панели
+    list_display = ('id', 'country_code_2_dig', )
+    # те поля, на которые мы можем кликнуть и перейти
+    list_display_links = ('id', 'country_code_2_dig')
+    # те поля, по которым мы можем производить поиск информации
+    search_fields = ('country_code_2_dig', )
+
+
+admin.site.register(CountriesAgainstRFRB, CountriesAgainstRFRBAdmin)
+
+
+class ResultTableAdmin(admin.ModelAdmin):
+    # список полей, которые мы хотим видеть в нашей админ панели
+    list_display = ('id', 'tn_ved_code', 'sanctions_tn_ved_code', 'country_code_2_dig', )
+    # те поля, на которые мы можем кликнуть и перейти
+    list_display_links = ('id', 'tn_ved_code')
+    # те поля, по которым мы можем производить поиск информации
+    search_fields = ('tn_ved_code', 'sanctions_tn_ved_code', 'country_code_2_dig', )
+
+
+admin.site.register(ResultTable, ResultTableAdmin)
--- sanctions_tn_ved_codes/tn_ved_main/models.py
+++ sanctions_tn_ved_codes/tn_ved_main/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
-# Create your models here.
+
+class ActualTnvedDirectory(models.Model):
+    tn_ved_code = models.CharField('Номер кода', max_length=15)
+    tn_ved_code_description = models.TextField('Описание кода', blank=True, default='Description',)
+
+    def __str__(self):
+        return str(self.tn_ved_code)
+
+    class Meta:
+        verbose_name = 'Справочник кодов ТН ВЭД'
+        verbose_name_plural = 'Справочник кодов ТН ВЭД'
+
+
+class ActualSanctionsTnvedCodes(models.Model):
+    sanctions_tn_ved_code = models.CharField('Номер санкционного кода', max_length=15)
+    sanctions_tn_ved_code_description = models.TextField('Описание санкционного кода',
+                                                         blank=True, default='Description')
+    country_code_2_dig = models.ForeignKey('CountriesAgainstRFRB', null=True, on_delete=models.CASCADE,
+                                           verbose_name="Код страны (2 символа)")
+
+    def __str__(self):
+        return str(self.sanctions_tn_ved_code)
+
+    class Meta:
+        verbose_name = 'Справочник санкционных кодов ТН ВЭД'
+        verbose_name_plural = 'Справочник санкционных кодов ТН ВЭД'
+
+
+class CountriesAgainstRFRB(models.Model):
+    country_code_2_dig = models.CharField('Код страны (2 символа)', max_length=15)
+
+    # magic method - возвращает имя категории
+    def __str__(self):
+        return self.country_code_2_dig
+
+    # def get_absolute_url(self):
+    #     return reverse('category', kwargs={'cat_slug': self.slug})
+
+    class Meta:
+        verbose_name = 'Наименование страны'
+        verbose_name_plural = 'Наименование стран'
+
+
+class ResultTable(models.Model):
+    tn_ved_code = models.ForeignKey('ActualTnvedDirectory', null=True, on_delete=models.CASCADE,
+                                    verbose_name="Код ТН ВЭД")
+    # tn_ved_code_description = models.ForeignKey('ActualTnvedDirectory', null=True, on_delete=models.CASCADE,
+    #                                             verbose_name="Описание кода ТН ВЭД")
+    sanctions_tn_ved_code = models.ForeignKey('ActualSanctionsTnvedCodes', null=True, on_delete=models.CASCADE,
+                                              verbose_name="Санкционный код ТН ВЭД")
+    country_code_2_dig = models.ForeignKey('CountriesAgainstRFRB', null=True, on_delete=models.CASCADE,
+                                           verbose_name="Страна, которая ввела санкции")
+
+    class Meta:
+        verbose_name = 'Итоговая таблица'
+        verbose_name_plural = 'Итоговая таблица'
