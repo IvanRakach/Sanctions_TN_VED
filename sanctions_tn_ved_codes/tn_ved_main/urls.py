46ddc45b27bd639106546f17a6bd223107057340
Ivan <ivanrakach@gmail.com>
2022-04-02 14:08:20 +0300

    - search have been upgraded (codes and string descriptions are available for searching now)

--- sanctions_tn_ved_codes/tn_ved_main/urls.py
+++ sanctions_tn_ved_codes/tn_ved_main/urls.py
@@ -5,5 +5,6 @@ from .views import *
 urlpatterns = [
     path('', views.index, name="home"),
     path('search_results/', SearchResultsView.as_view(), name='search_results'),
-    # path('search/', views.show_search_results, name='search_results'),
+    # path('search_results/', views.show_search_results, name='search_results'),
+    path('usa_sanctions_list', views.show_usa_sanctions_list, name='usa_sanctions_list')
 ]
--- sanctions_tn_ved_codes/tn_ved_main/views.py
+++ sanctions_tn_ved_codes/tn_ved_main/views.py
@@ -29,12 +29,25 @@ class SearchResultsView(ListView):
     model = SanctionsTnvedCodes  # !!!!!!!!!!!!!!!!!!!!!!
     template_name = 'sanctionstnvedcodes_list.html'
 
-    def get_queryset(self):
+    # def get_queryset(self):
+    #     query = self.request.GET.get('q')
+    #     object_list = SanctionsTnvedCodes.objects.filter(sanctions_tn_ved_code=query)
+    #     return object_list
+
+    def get_queryset(self):  # новый
         query = self.request.GET.get('q')
-        object_list = SanctionsTnvedCodes.objects.filter(sanctions_tn_ved_code=query)
+        return SanctionsTnvedCodes.objects.filter(
+            # для определения рус символов их нужно преобразовать в единый формат
+            Q(sanctions_tn_ved_code__icontains=query) | Q(sanctions_tn_ved_code_description_rus__icontains=query) |
+            Q(sanctions_tn_ved_code_description_eng__icontains=query)
+            # Q(sanctions_tn_ved_code_description_rus__contains=query)
+        )
 
-        # query_2 = self.request.GET.get('q')
-        # object_list_2 = SanctionsTnvedCodes.objects.filter(sanctions_tn_ved_code=query_2)
 
-        return object_list
-
+def show_usa_sanctions_list(request):
+    us_sanctions_query = SanctionsTnvedCodes.objects.filter(country_code_2_dig_1side__contains="US")
+    # us_sanctions_query = SanctionsTnvedCodes.objects.filter(sanctions_tn_ved_code__contains="7225")
+    param_for_render = {
+        'us_sanctions_query': us_sanctions_query,
+    }
+    return render(request, 'tn_ved_main/USA_sanctions_list.html', context=param_for_render)
