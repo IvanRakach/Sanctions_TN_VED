8409581ccbc01f1fc24ac489eaefeaf69bea46fe
Ivan <ivanrakach@gmail.com>
2022-05-26 23:35:19 +0300

    PostgreSQL has been installed instead of SQLite

--- sanctions_tn_ved_codes/sanctions_tn_ved_codes/settings.py
+++ sanctions_tn_ved_codes/sanctions_tn_ved_codes/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -23,10 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)j=^-8g@11#rwef4if8u)74##$5q94*xlv3%)tnc*x+jemlp+_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False  # True False
+DEBUG = True  # True False
 
 ALLOWED_HOSTS = ['127.0.0.1', '10.0.254.9']
-
 
 # Application definition
 
@@ -73,17 +71,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'sanctions_tn_ved_codes.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '1111',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -103,7 +110,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -114,7 +120,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
@@ -134,7 +139,6 @@ MEDIA_URL = '/media/'
 #
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
 
 # Default primary key field type
--- sanctions_tn_ved_codes/tn_ved_main/models.py
+++ sanctions_tn_ved_codes/tn_ved_main/models.py
@@ -25,14 +25,14 @@ class SanctionsTnvedCodesManager(models.Manager):
 
 
 class SanctionsTnvedCodes(models.Model):
-    sanctions_tn_ved_code = models.CharField('CN код', max_length=200, db_index=True)
+    sanctions_tn_ved_code = models.CharField('CN код', max_length=1500, db_index=True)
     sanctions_tn_ved_code_description_eng = models.TextField('Описание санкционного кода', )
     sanctions_tn_ved_code_description_rus = models.TextField('Описание санкционного кода (рус.)', )
     type_of_restrictions = models.TextField('Дополнительное пояснение', )
-    country_code_2_dig_1side = models.CharField('Страна отправления', max_length=2)
-    country_code_2_dig_2side = models.CharField('Страна назначения', max_length=2)
+    country_code_2_dig_1side = models.CharField('Страна отправления', max_length=6)
+    country_code_2_dig_2side = models.CharField('Страна назначения', max_length=6)
     sanctions_initiator_country_code_2_dig = models.CharField('Страна, которая ввела санкции', max_length=2)
-    export_import_ru = models.CharField('Экспорт в РФ / Импорт в РФ', max_length=12)
+    export_import_ru = models.CharField('Экспорт в РФ / Импорт в РФ', max_length=250)
     note = models.TextField('Примечание', default="-")
 
     objects = SanctionsTnvedCodesManager()
@@ -42,7 +42,7 @@ class SanctionsTnvedCodes(models.Model):
 
     class Meta:
         verbose_name = 'Список санкционных кодов стран "Западного мира"'
-        verbose_name_plural = 'Список санкционных Стран "Западного мира"'
+        verbose_name_plural = 'Список санкционных кодов стран "Западного мира"'
 
 
 class TnVedCodes(models.Model):
@@ -79,7 +79,7 @@ class SanctionsTnvedCodesFromRU311312313BY147Manager(models.Manager):
 
 
 class SanctionsTnvedCodesFromRU311312313BY147(models.Model):
-    sanctions_tn_ved_code = models.CharField('Код ТН ВЭД ЕАЭС', max_length=10, db_index=True)
+    sanctions_tn_ved_code = models.CharField('Код ТН ВЭД ЕАЭС', max_length=250, db_index=True)
     sanctions_tn_ved_code_description = models.TextField('Описание кода', )
     type_of_restrictions = models.TextField('Вид ограничений', )
     doc_name = models.TextField('Наименование документа', )
@@ -95,7 +95,7 @@ class SanctionsTnvedCodesFromRU311312313BY147(models.Model):
 
 
 class SanctionCountriesListFromRU313(models.Model):
-    sanction_country_rus = models.CharField('Наименование страны', max_length=20)
+    sanction_country_rus = models.CharField('Наименование страны', max_length=200)
     sanction_country_iso_alpha_2 = models.CharField('Alpha-2 ISO 3166-1', max_length=2, db_index=True)
     sanction_country_iso_alpha_3 = models.CharField('Alpha-3 ISO 3166-1', max_length=3, db_index=True)
     eu_member_country = models.CharField('Страна-член ЕС', max_length=2)
